a388918d75ef45a570be82f91695232019d8a0bb
Holger Kraus <holger.kraus@innoq.com>
2020-04-19 16:21:27 +0200

    fix: improvements due to review comments

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,22 @@ const profile = document.querySelector(".profile");
 const profileName = profile.querySelector(".profile__name");
 const profileAboutMe = profile.querySelector(".profile__about-me");
 const editButton = profile.querySelector(".profile__edit-button");
-editButton.addEventListener("click", openPopup);
 
 // popup elements
 const popup = document.querySelector(".popup");
 const fieldName = popup.querySelector(".form__field_name");
 const fieldAboutMe = popup.querySelector(".form__field_about-me");
-
 const cancelButton = popup.querySelector(".form__cancel");
-cancelButton.addEventListener("click", closePopup);
-
 const form = popup.querySelector(".form");
+
+// register listeners
+editButton.addEventListener("click", openPopup);
+cancelButton.addEventListener("click", closePopup);
 form.addEventListener("submit", submitForm);
 
 function openPopup() {
-  fieldName.value = profileName.innerText;
-  fieldAboutMe.value = profileAboutMe.innerText;
+  fieldName.value = profileName.textContent;
+  fieldAboutMe.value = profileAboutMe.textContent;
   popup.classList.add("popup_opened");
 }
 
@@ -28,8 +28,8 @@ function closePopup() {
 
 function submitForm(event) {
   event.preventDefault();
-  profileName.innerHTML =  fieldName.value;
-  profileAboutMe.innerHTML = fieldAboutMe.value;
+  profileName.textContent =  fieldName.value;
+  profileAboutMe.textContent = fieldAboutMe.value;
   closePopup();
 }
 
