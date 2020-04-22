2fc137b40c0bb192234cab731645aa481b498d77
Holger Kraus <holger.kraus@innoq.com>
2020-04-22 19:54:51 +0200

    feat: add initialisation of card elements

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,49 @@ function submitForm(event) {
 }
 
 
+function addElement(element) {
+  const elementTemplate = document.querySelector("#element__template").content;
+  const elementsList = document.querySelector(".elements__list");
+  const elementFragment = elementTemplate.cloneNode(true);
+  const newElement = elementFragment.querySelector(".element");
+
+  newElement.querySelector(".element__title").textContent = element.name;
+  newElement.style.backgroundImage = "url('" + element.link + "')";
+  elementsList.append(newElement);
+}
+
+function initializeCards() {
+
+  const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+      name: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+      name: "Vanois National Park",
+      link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+  ];
+
+  initialCards.forEach(element => addElement(element));
+}
+
+initializeCards();
+
+
