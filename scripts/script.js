333641c2537fe672708f9b5ba51b4f8709c0cb3a
Holger Kraus <holger.kraus@innoq.com>
2020-04-24 16:26:00 +0200

    feat: add image overlay including opening functionality

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,23 @@ const profileAboutMe = profile.querySelector(".profile__about-me");
 const editButton = profile.querySelector(".profile__edit-button");
 const addButton = profile.querySelector(".profile__add-button");
 
-// popup elements
-const profilePopup = document.querySelector(".popup");
+// overlay-place-profile elements
+const profilePopup = document.querySelector(".overlay-profile");
 const profileFieldName = profilePopup.querySelector(".form__field_title");
 const profileFieldAboutMe = profilePopup.querySelector(".form__field_detail");
 const profileCancelButton = profilePopup.querySelector(".form__cancel");
 const profileForm = profilePopup.querySelector(".form");
 
-// overlay elements
-const placePopup = document.querySelector(".overlay");
+// overlay-place elements
+const placePopup = document.querySelector(".overlay-place");
 const placeFieldTitle = placePopup.querySelector(".form__field_title");
 const placeFieldLink = placePopup.querySelector(".form__field_detail");
 const placeCancelButton = placePopup.querySelector(".form__cancel");
 const placeForm = placePopup.querySelector(".form");
+
+// overlay-image elements
+const imageOverlay = document.querySelector(".overlay-image");
+const imageOverlayCloseButton = imageOverlay.querySelector(".overlay-image_close");
 
 // register listeners
 editButton.addEventListener("click", openProfilePopup);
@@ -26,26 +30,27 @@ profileCancelButton.addEventListener("click", closeProfilePopup);
 profileForm.addEventListener("submit", submitForm);
 placeCancelButton.addEventListener("click", closePlacePopup);
 placeForm.addEventListener("submit", submitPlaceForm);
+imageOverlayCloseButton.addEventListener("click", closeImageOverlay);
 
 function openProfilePopup() {
   profileFieldName.value = profileName.textContent;
   profileFieldAboutMe.value = profileAboutMe.textContent;
-  profilePopup.classList.add("popup_opened");
+  profilePopup.classList.add("overlay-profile_opened");
 }
 
 function closeProfilePopup() {
-  profilePopup.classList.remove("popup_opened");
+  profilePopup.classList.remove("overlay-profile_opened");
 }
 
 function openPlacePopup() {
   placeFieldTitle.value = null;
   placeFieldLink.value = null;
-  placePopup.classList.add("overlay_opened");
+  placePopup.classList.add("overlay-place_opened");
 
 }
 
 function closePlacePopup() {
-  placePopup.classList.remove("overlay_opened");
+  placePopup.classList.remove("overlay-place_opened");
 }
 
 function submitForm(event) {
@@ -75,11 +80,27 @@ function removeElement(event) {
   removeButton.parentElement.remove();
 }
 
+function closeImageOverlay() {
+  imageOverlay.classList.remove("overlay-image_opened");
+}
+
+function openImageOverlay(event) {
+  const selectedImage = event.target;
+  const selectedImageTitle = selectedImage.querySelector(".element__title");
+  const image = imageOverlay.querySelector(".image");
+  image.src = selectedImage.style.backgroundImage.slice(5, -2);
+  const imageTitle = imageOverlay.querySelector(".image__title");
+  imageTitle.textContent = selectedImageTitle.textContent;
+  imageOverlay.classList.add("overlay-image_opened");
+}
+
 function addElement(element) {
   const elementTemplate = document.querySelector("#element__template").content;
   const elementsList = document.querySelector(".elements__list");
   const elementFragment = elementTemplate.cloneNode(true);
+
   const newElement = elementFragment.querySelector(".element");
+  newElement.addEventListener("click", openImageOverlay);
 
   newElement.querySelector(".element__title").textContent = element.name;
   newElement.style.backgroundImage = "url('" + element.link + "')";
